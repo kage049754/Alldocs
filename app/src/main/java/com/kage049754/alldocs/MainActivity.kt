a7ed92d5575c5af42f5ca26f3044ef49401bcd02
Taylee Henry <kage049754@gmail.com>
2026-09-23 14:40:47 +0800

    Wire application ViewModel

--- app/src/main/java/com/kage049754/alldocs/MainActivity.kt
+++ app/src/main/java/com/kage049754/alldocs/MainActivity.kt
@@ -3,8 +3,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kage049754.alldocs.ui.AlldocsApp
+import com.kage049754.alldocs.ui.AppViewModel
 import com.kage049754.alldocs.ui.theme.AlldocsTheme
-class MainActivity: ComponentActivity(){
- override fun onCreate(savedInstanceState:Bundle?){super.onCreate(savedInstanceState);enableEdgeToEdge();setContent{AlldocsTheme{AlldocsApp()}}}
-}
+class MainActivity:ComponentActivity(){override fun onCreate(b:Bundle?){super.onCreate(b);enableEdgeToEdge();setContent{AlldocsTheme{val vm:AppViewModel=viewModel(factory=AppViewModel.factory(applicationContext));AlldocsApp(vm)}}}}
