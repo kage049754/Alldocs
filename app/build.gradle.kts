81e63d28e67109325d7cc3b46583f1c11fc09019
Taylee Henry <kage049754@gmail.com>
2026-09-23 14:52:55 +0800

    Add offline PDF processing dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,4 +31,5 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.tom-roush:pdfbox-android:2.0.27.0")
 }
