9470e7deab4eac8ece774cb9075f80a0e93d902f
Taylee Henry <kage049754@gmail.com>
2026-09-23 14:44:43 +0800

    Fix Activity Compose wiring

--- app/src/main/java/com/kage049754/alldocs/MainActivity.kt
+++ app/src/main/java/com/kage049754/alldocs/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.kage049754.alldocs
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,4 +8,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kage049754.alldocs.ui.AlldocsApp
 import com.kage049754.alldocs.ui.AppViewModel
 import com.kage049754.alldocs.ui.theme.AlldocsTheme
-class MainActivity:ComponentActivity(){override fun onCreate(b:Bundle?){super.onCreate(b);enableEdgeToEdge();setContent{AlldocsTheme{val vm:AppViewModel=viewModel(factory=AppViewModel.factory(applicationContext));AlldocsApp(vm)}}}}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val vm: AppViewModel = viewModel(factory = AppViewModel.factory(applicationContext))
+            AlldocsTheme {
+                AlldocsApp(vm)
+            }
+        }
+    }
+}
