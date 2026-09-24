85ed98cff6adc89decd40b9fbfb6583151571192
Taylee Henry <kage049754@gmail.com>
2026-09-24 13:28:03 +0800

    Fix DOCX line break export

--- app/src/main/java/com/kage049754/alldocs/io/DocxWriter.kt
+++ app/src/main/java/com/kage049754/alldocs/io/DocxWriter.kt
@@ -11,7 +11,7 @@ object DocxWriter {
         ZipOutputStream(out).use { zip ->
             entry(zip, "[Content_Types].xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Types xmlns=\"http://schemas.openxmlformats.org/package/2006/content-types\"><Default Extension=\"rels\" ContentType=\"application/vnd.openxmlformats-package.relationships+xml\"/><Default Extension=\"xml\" ContentType=\"application/xml\"/><Override PartName=\"/word/document.xml\" ContentType=\"application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml\"/></Types>")
             entry(zip, "_rels/.rels", "<?xml version=\"1.0\" encoding=\"UTF-8\"?><Relationships xmlns=\"http://schemas.openxmlformats.org/package/2006/relationships\"><Relationship Id=\"rId1\" Type=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument\" Target=\"word/document.xml\"/></Relationships>")
-            val paragraphs = body.split("\\n").joinToString("") { p -> "<w:p><w:r><w:t xml:space=\"preserve\">${escape(p)}</w:t></w:r></w:p>" }
+            val paragraphs = body.replace("\r\n", "\n").replace("\r", "\n").split("\n").joinToString("") { p -> "<w:p><w:r><w:t xml:space=\"preserve\">${escape(p)}</w:t></w:r></w:p>" }
             entry(zip, "word/document.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?><w:document xmlns:w=\"http://schemas.openxmlformats.org/wordprocessingml/2006/main\"><w:body>$paragraphs<w:sectPr><w:pgSz w:w=\"12240\" w:h=\"15840\"/><w:pgMar w:top=\"1440\" w:right=\"1440\" w:bottom=\"1440\" w:left=\"1440\"/></w:sectPr></w:body></w:document>")
         }
     }
